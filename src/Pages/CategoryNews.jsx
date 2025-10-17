e5611e4a03c97e264994991319dabf36d3ff5417
shams al labib <samslabib19@gmail.com>
2025-10-18 01:00:54 +0600

    showed details card data in screen 50-8

--- src/Pages/CategoryNews.jsx
+++ src/Pages/CategoryNews.jsx
@@ -1,5 +1,6 @@
 import React, {  useEffect, useState  } from 'react';
 import { useLoaderData, useParams } from 'react-router';
+import NewsCard from './NewsCard';
 
 const CategoryNews = () => {
 
@@ -31,7 +32,13 @@ const CategoryNews = () => {
 
     return (
         <div>
-            <h1>Category - {categoryNews.length} Found</h1>
+            <h1 className='font-bold mb-5'>Total - <span className='text-secondary'>{categoryNews.length}</span> data Found</h1>
+
+            <div className='grid grid-cols-1 gap-5'>
+                  {
+                    categoryNews.map(news => <NewsCard key={news.id} news={news}></NewsCard>)
+                  }
+            </div>
         </div>
     );
 };
