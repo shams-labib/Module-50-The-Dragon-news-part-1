785fef4fc2705fa16897f957f483f3ed2165593d
shams al labib <samslabib19@gmail.com>
2025-10-18 01:25:43 +0600

    added rightside section

--- src/layout/Aside/RightAside/RightAside.jsx
+++ src/layout/Aside/RightAside/RightAside.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import SocialLogin from './SocialLogin';
+import FindUs from './FindUs';
+import Qzone from './Qzone';
 
 const RightAside = () => {
     return (
-        <div className=''>
-           <SocialLogin></SocialLogin>
+        <div className='space-y-8'>
+           <div>
+            <SocialLogin></SocialLogin>
+           </div>
+           <FindUs></FindUs>
+           <Qzone></Qzone>
         </div>
     );
 };
